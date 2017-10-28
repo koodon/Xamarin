31ee2cb5eb823d71b695683931bb7f6be9ba42ae
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-10-28 11:03:08 +0300

    Android

---
+++ Exercises/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Exercises
+{
+    [Activity(Label = "Exercises", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var button1 = FindViewById <Button>(Resource.Id.button1);
+
+            button1.Click += delegate
+            {
+                StartActivity(typeof(SecondActivity));
+            };
+
+            var button2 = FindViewById<Button>(Resource.Id.button2);
+
+            var textview1 = FindViewById<TextView>(Resource.Id.textView1);
+
+            button2.Click += delegate
+            {
+                textview1.Text = "Uus tekst";
+            };
+        }
+    }
+}
+
